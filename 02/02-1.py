099affe7961d1b6852f1bd802192ef669f012ffa
xiaoxiae <tomas.slama.131@gmail.com>
2020-12-02 10:12:27 +0100

    problem 2

---
+++ 02/02-1.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, "../")
+from utilities import success, get_input
+
+input = get_input()
+total = 0
+
+for value in input:
+    a, b, password = value.split()
+    lo, hi = map(int, a.split("-"))
+    policy = b.strip(":")
+
+    if lo <= password.count(policy) <= hi:
+        total += 1
+
+print(total)
---
+++ 02/02-2.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.insert(0, "../")
+from utilities import success, get_input
+
+input = get_input()
+total = 0
+
+for value in input:
+    a, b, password = value.split()
+    i, j = map(int, a.split("-"))
+    i -= 1
+    j -= 1
+    policy = b.strip(":")
+
+    if (password[i] == policy or password[j] == policy) and password[i] != password[j]:
+        total += 1
+
+print(total)
